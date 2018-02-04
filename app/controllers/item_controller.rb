587da4394fcb0732a694a1d4b555162b357a7bf0
chgchg123 <chgchg123@naver.com>
2018-02-04 04:17:18 -0500

    styleing up

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -3,12 +3,12 @@ require 'json'
 
 class ItemController < ApplicationController
   def index
-    num_items = 0;
+    num_items = 0
     @items = []
     list = JSON.parse(open("https://hacker-news.firebaseio.com/v0/topstories.json").read)
     30.times do |i|
       @items.push(JSON.parse(open("https://hacker-news.firebaseio.com/v0/item/" + list[i].to_s + ".json").read))
-      
+      num_items = num_items + 1
     end
   end
 end

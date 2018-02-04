636919dfcf953ab7fd0652a3f5755c77ebb86809
chgchg123 <chgchg123@naver.com>
2018-02-04 03:33:51 -0500

    initalized json load

---
+++ app/controllers/item_controller.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+require 'json'
+
+class ItemController < ApplicationController
+  def index
+    num_items = 0;
+    @items = []
+    list = JSON.parse(open("https://hacker-news.firebaseio.com/v0/topstories.json").read)
+    30.times do |i|
+      @items.push(JSON.parse(open("https://hacker-news.firebaseio.com/v0/item/" + list[i].to_s + ".json").read))
+      
+    end
+  end
+end
